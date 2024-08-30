691c03d12faeceb762a7b96d7fd2d7827c5028ae
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2024-08-31 00:48:03 +0200

    Creation des components et configuration des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+        },
+        {
+            path: 'calendar',
+            component: CalendarComponent
+            }
+
+];
