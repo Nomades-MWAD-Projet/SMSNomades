ce2519749739c843c464d4a15fcd1b95ad883caa
Blendina <ramiziblendina8@mail.com>
2024-09-03 12:06:08 +0200

    first commit

---
+++ src/app/components/create-zoom/create-zoom.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-zoom',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-zoom.component.html',
+  styleUrl: './create-zoom.component.css'
+})
+export class CreateZoomComponent {
+
+}
