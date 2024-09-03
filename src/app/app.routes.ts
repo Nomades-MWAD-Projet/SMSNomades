ee1621b63dd9677b89dec0bb7956bc98a861acc9
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2024-09-03 10:24:22 +0200

    login.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,19 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./components/login/login.component";
+import { CalendarComponent } from "./components/calendar/calendar.component";
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-    },
-    {
-        path: 'home',
-        component: HomeComponent
-        },
-        {
-            path: 'calendar',
-            component: CalendarComponent
-            }
-
+  {
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full",
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "calendar",
+    component: CalendarComponent,
+  },
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: true,
   imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
 })
 export class HomeComponent {
 
