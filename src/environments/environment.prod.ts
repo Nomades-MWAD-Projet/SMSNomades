53680e66a0479b81115c373a0a6588de7e67437f
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2024-09-10 11:26:47 +0200

    chore: Update firebase configuration in environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyDA9pyvIg-fcFNj1iHgthtM1If0-qjkPOc", 
+      authDomain: "nomades-4c625.firebaseapp.com",
+      projectId: "nomades-4c625",
+      storageBucket: "nomades-4c625.appspot.com",
+      messagingSenderId: "520546108314",
+      appId: "1:520546108314:web:6052bc5f0bfc23dca8f023",
+      databaseURL: "https://nomades-4c625-default-rtdb.europe-west1.firebasedatabase.app/"
+    }
+  };
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideDatabase, getDatabase } from '@angular/fire/database';
-import { environment } from './environments/environment'; 
+import { environment } from '../src/environments/environment'; 
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
