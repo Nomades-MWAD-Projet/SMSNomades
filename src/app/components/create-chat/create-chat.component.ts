8d32c535a10b9935c0b11849d9f530d51533d478
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2024-09-10 16:46:18 +0200

    chore: Update menu component HTML and CSS formatting

--- src/app/components/create-chat/create-chat.component.ts
+++ src/app/components/create-chat/create-chat.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core'; // Importer OnInit
-import { FormsModule } from '@angular/forms'; // Pour ngModel
-import { ChatService } from './create-chat.service';
-import { CommonModule, DatePipe } from '@angular/common'; // Pour formater les dates
+import { Component, OnInit } from "@angular/core"; // Importer OnInit
+import { FormsModule } from "@angular/forms"; // Pour ngModel
+import { ChatService } from "./create-chat.service";
+import { CommonModule, DatePipe } from "@angular/common"; // Pour formater les dates
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-create-chat',
+  selector: "app-create-chat",
   standalone: true,
   imports: [FormsModule, DatePipe, CommonModule],
-  templateUrl: './create-chat.component.html',
-  styleUrls: ['./create-chat.component.css']
+  templateUrl: "./create-chat.component.html",
+  styleUrls: ["./create-chat.component.css"],
 })
-export class CreateChatComponent implements OnInit { // Implémenter OnInit
-  newMessage: string = '';
-  roomId: string = 'roomId1'; // Exemple de salle de chat
-  messages$ : any;  // Initialisation après l'injection du service
+export class CreateChatComponent implements OnInit {
+  // Implémenter OnInit
+  newMessage: string = "";
+  roomId: string = "roomId1"; // Exemple de salle de chat
+  messages$: Observable<any[]>; // Initialisation après l'injection du service
 
   constructor(public chatService: ChatService) {
     this.messages$ = chatService.messages$;
@@ -28,7 +30,7 @@ export class CreateChatComponent implements OnInit { // Implémenter OnInit
   sendMessage(): void {
     if (this.newMessage.trim()) {
       this.chatService.sendMessage(this.roomId, this.newMessage); // Appeler la méthode pour envoyer un message
-      this.newMessage = '';  // Vider le champ de message après l'envoi
+      this.newMessage = ""; // Vider le champ de message après l'envoi
     }
   }
 }
