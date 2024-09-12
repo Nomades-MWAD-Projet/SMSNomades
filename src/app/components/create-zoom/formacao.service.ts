452cd3547afe818f451e315791afe35f7fadef42
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2024-09-12 04:06:41 +0200

    chore: Add Formacao interface and update FormacaoService

--- src/app/components/create-zoom/formacao.service.ts
+++ src/app/components/create-zoom/formacao.service.ts
@@ -1,5 +1,6 @@
 
 import { Injectable } from '@angular/core';
+import { Formacao } from '../../interfaces/formacoes';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { provideFirebaseApp, initializeApp } from "@angular/fire/app";
 import { provideAuth, getAuth } from "@angular/fire/auth";
 import { provideDatabase, getDatabase } from "@angular/fire/database";
-import { environment } from "../src/environments/environment";
+import { firebaseConfig } from "../src/environments/environment";
 import { AppComponent } from "./app/app.component";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { appConfig } from "./app/app.config";
